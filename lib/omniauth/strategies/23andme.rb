527a796febc405b003f5d40c7f59686e6e7adf32
Bernardo Gurgel de Faria <bernardogfilho@gmail.com>
2017-01-25 11:35:57 -0300

    Some code styling refactoring

--- lib/omniauth/strategies/23andme.rb
+++ lib/omniauth/strategies/23andme.rb
@@ -3,13 +3,15 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class TwentyThreeAndMe < OmniAuth::Strategies::OAuth2
-      option :name, "twenty_three_and_me"
+      option :name, 'twenty_three_and_me'
+
       option :client_options, {
-        :site => 'https://api.23andme.com',
-        :authorize_url => 'https://api.23andme.com/authorize',
-        :token_url => 'https://api.23andme.com/token'
+        site: 'https://api.23andme.com',
+        authorize_url: 'https://api.23andme.com/authorize',
+        token_url: 'https://api.23andme.com/token'
       }
-      option :authorize_params, {grant_type: 'authorization_code'}
+
+      option :authorize_params, grant_type: 'authorization_code'
 
       def request_phase
         puts authorize_params
@@ -18,26 +20,23 @@ module OmniAuth
 
       def authorize_params
         super.tap do |params|
-          if request.params['scope']
-            params[:scope] = request.params['scope']
-          end
+          params[:scope] = request.params['scope'] if request.params['scope']
         end
       end
 
       uid { raw_info['id'].to_s }
 
       info do
-        {id:raw_info['id']}
+        { id: raw_info['id'] }
       end
 
       extra do
-        {:raw_info => raw_info}
+        { raw_info: raw_info }
       end
 
       def raw_info
-        @raw_info ||= access_token.get("/1/user").parsed
+        @raw_info ||= access_token.get('/1/user').parsed
       end
-
     end
   end
 end
